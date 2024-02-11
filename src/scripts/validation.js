d8f11c552c40b2b432bf05e86ca776e1aafc3120
Olga Markevich <pyrkoolga@gmail.com>
2024-02-11 21:03:20 +0300

    feat: add validation

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -12,34 +12,93 @@ const phoneError = document.querySelector('#phone + .error-line');
 const message = document.getElementById('message');
 const messageError = document.querySelector('#message + .error-line');
 
+const serverMessage = document.querySelector('.server-message');
+
+const formBtn = document.getElementById('form-btn');
+
 email.addEventListener('input', function (event) {
+  serverMessage.innerHTML = '';
   if (email.validity.valid) {
     emailError.textContent = '';
     emailError.className = 'error';
+
+    if (
+      email.validity.valid &&
+      firstname.validity.valid &&
+      phone.validity.valid &&
+      message.validity.valid
+    ) {
+      formBtn.disabled = false;
+    }
   } else {
+    formBtn.disabled = true;
     showError();
   }
 });
 
+phone.addEventListener('input', function (event) {
+  serverMessage.innerHTML = '';
+  if (phone.validity.valid) {
+    phoneError.textContent = '';
+    phoneError.className = 'error';
+
+    if (
+      email.validity.valid &&
+      firstname.validity.valid &&
+      phone.validity.valid &&
+      message.validity.valid
+    ) {
+      formBtn.disabled = false;
+    }
+  } else {
+    formBtn.disabled = true;
+    showErrorPhone();
+  }
+});
+
 message.addEventListener('input', function (event) {
+  serverMessage.innerHTML = '';
   if (message.validity.valid) {
     messageError.textContent = '';
     messageError.className = 'error';
+
+    if (
+      email.validity.valid &&
+      firstname.validity.valid &&
+      phone.validity.valid &&
+      message.validity.valid
+    ) {
+      formBtn.disabled = false;
+    }
   } else {
+    formBtn.disabled = true;
     showErrorMessage();
   }
 });
 
 firstname.addEventListener('input', function (event) {
+  firstname.classList.add('durty');
+  serverMessage.innerHTML = '';
   if (firstname.validity.valid) {
     firstnameError.textContent = '';
     firstnameError.className = 'error';
+
+    if (
+      email.validity.valid &&
+      firstname.validity.valid &&
+      phone.validity.valid &&
+      message.validity.valid
+    ) {
+      formBtn.disabled = false;
+    }
   } else {
+    formBtn.disabled = true;
     showErrorFirstname();
   }
 });
 
 function showError() {
+  email.classList.add('durty');
   if (email.validity.valueMissing) {
     emailError.textContent = 'Enter your email';
   } else if (email.validity.typeMismatch) {
@@ -51,6 +110,8 @@ function showError() {
 }
 
 function showErrorMessage() {
+  serverMessage.innerHTML = '';
+  message.classList.add('durty');
   if (message.validity.valueMissing) {
     messageError.textContent = 'Enter your message';
   } else if (message.validity.typeMismatch) {
@@ -61,22 +122,66 @@ function showErrorMessage() {
   messageError.className = 'error active';
 }
 
+function showErrorPhone() {
+  phone.classList.add('durty');
+  serverMessage.innerHTML = '';
+  if (phone.validity.valueMissing) {
+    phoneError.textContent = 'Enter your message';
+  } else if (phone.validity.typeMismatch) {
+    phoneError.textContent = 'Entered value needs to be an e-mail address.';
+  } else if (phone.validity.tooShort) {
+    phoneError.textContent = `Message should be at least ${phone.minLength} characters; you entered ${phone.value.length}.`;
+  }
+  phoneError.className = 'error active';
+}
+
 function showErrorFirstname() {
+  firstname.classList.add('durty');
+  serverMessage.innerHTML = '';
   if (firstname.validity.valueMissing) {
     firstnameError.textContent = 'Enter your Firstname';
   }
   firstnameError.className = 'error active';
 }
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
   e.preventDefault();
-
   if (
     email.validity.valid &&
     firstname.validity.valid &&
     phone.validity.valid &&
     message.validity.valid
   ) {
-    console.log(email.value, firstname.value, phone.value, message.value);
+    const user = {
+      email: email.value,
+      firstname: firstname.value,
+      phone: phone.value,
+      message: message.value,
+    };
+
+    const response = await fetch('http://localhost:9090/api/registration', {
+      method: 'POST',
+      body: JSON.stringify(user),
+    });
+
+    const result = await response.json();
+
+    if (result.status === 'success') {
+      serverMessage.innerText = result.message;
+      serverMessage.classList.remove('error-m');
+      serverMessage.classList.add('success');
+      phone.value = '';
+      message.value = '';
+      firstname.value = '';
+      email.value = '';
+      phone.classList.remove('durty');
+      firstname.classList.remove('durty');
+      message.classList.remove('durty');
+      email.classList.remove('durty');
+    } else if (result.status === 'error') {
+      serverMessage.innerText = result.message;
+      serverMessage.classList.remove('success');
+      serverMessage.classList.add('error-m');
+    }
   }
 });
